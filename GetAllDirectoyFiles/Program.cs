6ef0966314d979a8fd35851e290358e42d22bcda
Vaibhav Pathade <vaibhavpathade4@gmail.com>
2021-06-21 23:04:25 +0530

    Add project files.

---
+++ GetAllDirectoyFiles/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace GetAllDirectoyFiles
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Enter Folder Path: ");
+            string path = Console.ReadLine();
+            string[] allFiles = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+            await PrintAllFiles(allFiles);
+        }
+
+
+        public static async Task PrintAllFiles(string[] files)
+        {
+            Parallel.ForEach(files, p =>
+            {
+                 Console.WriteLine(p);
+            });
+            await Task.CompletedTask;
+        }
+    }
+}
